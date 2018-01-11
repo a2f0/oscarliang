e517e1ee5a1c7c25ff2cb35325cf3f09b0182e4e
Dan Sullivan <dansullivan@gmail.com>
2018-01-11 04:40:11 +0000

    debug logging

--- multibyte/src/OscarLiang.ino
+++ multibyte/src/OscarLiang.ino
@@ -1,18 +1,38 @@
 #include <Wire.h>
 
 #define SLAVE_ADDRESS 0x04
+#define DEBUG false 
+#define Output SerialUSB
+
 int number = 0;
 int state = 0;
 
+void debugln(String s) {
+  if (DEBUG == true) {
+    Output.println(s);
+  }
+}
+
+void debug(String s) {
+  if (DEBUG == true) {
+    Output.println(s);
+  }
+}
+
 void setup() {
-  Serial.begin(9600); // start serial for output
+  //if DEBUG is set to true a serial monitor MUST be connected or the execution
+  //will be blocked 
+  if (DEBUG == true) { 
+    Output.begin(9600); // start serial for output
+    while (!Output);    // Wait for Serial monitor to open
+    debugln("Finished initializing serial");
+  }
   // initialize i2c as slave
   Wire.begin(SLAVE_ADDRESS);
 
   // define callbacks for i2c communication
   Wire.onReceive(receiveData);
   Wire.onRequest(sendData);
-  Serial.println("Ready!");
 }
 
 void loop() {
